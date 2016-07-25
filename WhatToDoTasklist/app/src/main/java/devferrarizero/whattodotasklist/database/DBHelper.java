2d93a2efaf270dc4471b1de041e6b537e34f3970
ferrarizero <kompanybbc@gmail.com>
2016-07-25 12:23:10 +0300

    Finished 7th lesson!

--- WhatToDoTasklist/app/src/main/java/devferrarizero/whattodotasklist/database/DBHelper.java
+++ WhatToDoTasklist/app/src/main/java/devferrarizero/whattodotasklist/database/DBHelper.java
@@ -30,6 +30,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     public static final String SELECTION_STATUS = DBHelper.TASK_STATUS_COLUMN + " = ?";
+    public static final String SELECTION_TIME_STAMP = TASK_TIME_STAMP_COLUMN + " = ?";
 
     private DBQueryManager queryManager;
     private DBUpdateManager updateManager;
@@ -70,5 +71,9 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public DBUpdateManager update() {
         return updateManager;
+    }
+
+    public void removeTask(long timeStamp){
+        getWritableDatabase().delete(TASKS_TABLE, SELECTION_TIME_STAMP, new String[]{Long.toString(timeStamp)});
     }
 }
